9d4580826bc93c01e11fac2f0db3fc54a44d4898
postsovietrepost <71707828+postsovietrepost@users.noreply.github.com>
2021-03-11 22:29:31 +0300

    Update character.js

--- js/character.js
+++ js/character.js
@@ -1,29 +1,3 @@
-// $(document).ready(function(){
-//   $("#sh").click(function() {
-//           var text = $(this).html();
-//       $('<img src="https://psv4.userapi.com/c520036/u509127371/docs/d27/f91d7da31025/shpicture.png?extra=bIwUcNjTSj5gcKnfw6RIrhS6-hN4TWQJZyBo8jtp_eLf8OS7P_fZFdv9T4eOPLOhADP8RfCv-mRyNlWLpo14uCxaSvtIh3UO48IJrjz3VTODO9B9_srqmXp8fM9EKr-XRfkdBIYY0pOpJO2eTxoysMv9Wg" alt="">').appendTo($(this));
-//       });
-//       $(function (){
-//         $('#sh').click(function(){
-//             // $('#sh').remove()
-//             $("#sh").add('<img src="https://psv4.userapi.com/c520036/u509127371/docs/d27/f91d7da31025/shpicture.png?extra=bIwUcNjTSj5gcKnfw6RIrhS6-hN4TWQJZyBo8jtp_eLf8OS7P_fZFdv9T4eOPLOhADP8RfCv-mRyNlWLpo14uCxaSvtIh3UO48IJrjz3VTODO9B9_srqmXp8fM9EKr-XRfkdBIYY0pOpJO2eTxoysMv9Wg" alt="">')
-//         });
-//     });
-//       })
-
-//
-// $(document).ready(function(){
-//       $('#sh').click(function(){
-//         if (!$(this).data('status')) {
-//           $(this).html('');
-//           $(this).data('status', true);
-//         }
-//         else {
-//           $(this).text($('#sh'));
-//           $(this).data('status', false);
-//         }
-//       });
-// })
 
 $(document).ready(function(){
   $('.shtxt').click(
@@ -45,4 +19,164 @@ $(document).ready(function(){
           $('.shcartinka').css('visibility', 'hidden')
       },
   );
+
+  $('.iitxt').click(
+      function(){
+          $('.iitxt').toggleClass('.iicartinka').removeClass('.iitxt');
+      },
+      function(){
+          $('.iicartinka').css('visibility', 'visible')
+          $('.iitxt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.iicartinka').click(
+      function(){
+          $('.iicartinka').toggleClass('.iitxt').removeClass('.iicartinka');
+      },
+      function(){
+          $('.iitxt').css('visibility', 'visible')
+          $('.iicartinka').css('visibility', 'hidden')
+      },
+  )
+  $('.ftxt').click(
+      function(){
+          $('.ftxt').toggleClass('.fcartinka').removeClass('.ftxt');
+      },
+      function(){
+          $('.fcartinka').css('visibility', 'visible')
+          $('.ftxt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.fcartinka').click(
+      function(){
+          $('.fcartinka').toggleClass('.ftxt').removeClass('.fcartinka');
+      },
+      function(){
+          $('.ftxt').css('visibility', 'visible')
+          $('.fcartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.rtxt').click(
+      function(){
+          $('.rtxt').toggleClass('.rcartinka').removeClass('.rtxt');
+      },
+      function(){
+          $('.rcartinka').css('visibility', 'visible')
+          $('.rtxt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.rcartinka').click(
+      function(){
+          $('.rcartinka').toggleClass('.rtxt').removeClass('.rcartinka');
+      },
+      function(){
+          $('.rtxt').css('visibility', 'visible')
+          $('.rcartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.t1txt').click(
+      function(){
+          $('.t1txt').toggleClass('.t1cartinka').removeClass('.t1txt');
+      },
+      function(){
+          $('.t1cartinka').css('visibility', 'visible')
+          $('.t1txt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.t1cartinka').click(
+      function(){
+          $('.t1cartinka').toggleClass('.t1txt').removeClass('.t1cartinka');
+      },
+      function(){
+          $('.t1txt').css('visibility', 'visible')
+          $('.t1cartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.etxt').click(
+      function(){
+          $('.etxt').toggleClass('.ecartinka').removeClass('.etxt');
+      },
+      function(){
+          $('.ecartinka').css('visibility', 'visible')
+          $('.etxt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.ecartinka').click(
+      function(){
+          $('.ecartinka').toggleClass('.etxt').removeClass('.ecartinka');
+      },
+      function(){
+          $('.etxt').css('visibility', 'visible')
+          $('.ecartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.t2txt').click(
+      function(){
+          $('.t2txt').toggleClass('.t2cartinka').removeClass('.t2txt');
+      },
+      function(){
+          $('.t2cartinka').css('visibility', 'visible')
+          $('.t2txt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.t2cartinka').click(
+      function(){
+          $('.t2cartinka').toggleClass('.t2txt').removeClass('.t2cartinka');
+      },
+      function(){
+          $('.t2txt').css('visibility', 'visible')
+          $('.t2cartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.t3txt').click(
+      function(){
+          $('.t3txt').toggleClass('.t3cartinka').removeClass('.t3txt');
+      },
+      function(){
+          $('.t3cartinka').css('visibility', 'visible')
+          $('.t3txt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.t3cartinka').click(
+      function(){
+          $('.t3cartinka').toggleClass('.t3txt').removeClass('.t3cartinka');
+      },
+      function(){
+          $('.t3txt').css('visibility', 'visible')
+          $('.t3cartinka').css('visibility', 'hidden')
+      },
+  )
+
+  $('.atxt').click(
+      function(){
+          $('.atxt').toggleClass('.acartinka').removeClass('.atxt');
+      },
+      function(){
+          $('.acartinka').css('visibility', 'visible')
+          $('.atxt').css('visibility', 'hidden')
+      },
+  );
+
+  $('.acartinka').click(
+      function(){
+          $('.acartinka').toggleClass('.atxt').removeClass('.acartinka');
+      },
+      function(){
+          $('.atxt').css('visibility', 'visible')
+          $('.acartinka').css('visibility', 'hidden')
+      },
+  )
+
 })
